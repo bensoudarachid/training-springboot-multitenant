439bfccc8b0e9c99e1c17eb2d12e1f4934e87e6c
Bensouda Rachid <bensourachid@gmail.com>
2017-05-05 18:19:26 +0100

    push deploy whole app 5

--- PushDeployTrainingApp.sh
+++ PushDeployTrainingApp.sh
@@ -2,10 +2,12 @@ cd /d/RP/Tests/SpringBoot_Training
 git add .
 git commit -m "$*"
 git push httpsorigin master
+curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=GetRestTrainingAppFromGitlab\&token=d8g347rg375trft3dr34r8zr84t845gc58g5t
 cd /d/RP/Tests/ReactToDoExp2
-npm run buildserver
+npm run build
 git add .
 git commit -m "$*"
 git push httpsorigin master
-sleep 60
-curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=BuildDeployTrainingApp&token=dfgufgug348rt4784f934hr9hr3489sdf
+curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=GetNodeTrainingAppFromGitlab\&token=5t8r28gg0f8h47gfgjhgf8762gfd34rv28fg
+sleep 40
+curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=BuildDeployTrainingApp\&token=dfgufgug348rt4784f934hr9hr3489sdf
--- Test.sh
+++ Test.sh
@@ -1,1 +1,1 @@
-curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=BuildDeployTrainingApp&token=dfgufgug348rt4784f934hr9hr3489sdf
+curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=BuildDeployTrainingApp\&token=dfgufgug348rt4784f934hr9hr3489sdf
