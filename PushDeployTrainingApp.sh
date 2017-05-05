523c968ed94eeacc5ba1e8b91fa9685447ede79c
Bensouda Rachid <bensourachid@gmail.com>
2017-05-05 17:20:21 +0100

    push deploy whole app 4

--- PushDeployTrainingApp.sh
+++ PushDeployTrainingApp.sh
@@ -7,4 +7,5 @@ npm run build
 git add .
 git commit -m "$*"
 git push httpsorigin master
+sleep 60
 curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=BuildDeployTrainingApp&token=dfgufgug348rt4784f934hr9hr3489sdf
