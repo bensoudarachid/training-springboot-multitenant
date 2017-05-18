b0a5af46a5bdbb3313e22b99d7765b63e3850ad9
Bensouda Rachid <bensourachid@gmail.com>
2017-05-18 10:22:44 +0100

    test push deploy whole app. Now with pm2 and tomcat cluster. 2

--- PushDeployTrainingApp.sh
+++ PushDeployTrainingApp.sh
@@ -12,5 +12,3 @@ git push httpsorigin master
 curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=GetNodeTrainingAppFromGitlab\&token=5t8r28gg0f8h47gfgjhgf8762gfd34rv28fg
 sleep 40
 curl --insecure https://localhost:6767/jenkins/buildByToken/build?job=BuildDeployTrainingApp\&token=dfgufgug348rt4784f934hr9hr3489sdf
-C:\Programm\apache-ant-1.10.1\bin\ant.bat -f
- version.xml
